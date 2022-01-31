02b56c74a4f4d8113f13da0a26fa2b2756e7f3eb
txj-xyz <jfinelli3@gmail.com>
2022-01-31 09:06:54 -0700

    feat: rewrite with user prompts in console

--- medal.js
+++ medal.js
@@ -1,29 +1,52 @@
-require('dotenv').config()
+// require('dotenv').config()
 
-const { Medal } = require('medal-js');
-const { Webhook } = require('simple-discord-webhooks');
-const discord = new Webhook(process.env.WEBHOOK);
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-const options = { userId: Number(process.env.USERID), limit: 1 };
-const medal = new Medal(process.env.KEY);
+rl.question('Medal API Key: ', function (key) {
+    process.env.KEY = String(key);
+    rl.question('Medal User ID: ', function (user) {
+        process.env.USERID = String(user);
+        rl.question('Discord Webhook URL: ', function (user) {
+            process.env.WEBHOOK = String(user);
+            rl.close();
+            console.log('\n\nSETUP ENV CORRECTLY, STARTING MEDAL LISTENER!')
+            try {
+                const { Medal } = require('medal-js');
+                const { Webhook } = require('simple-discord-webhooks');
+                const discord = new Webhook(process.env.WEBHOOK);
 
-(async () => {
-    const i = await medal.latest(options);
-    let firstClipObj = i.data.contentObjects[0];
+                const options = { userId: Number(process.env.USERID), limit: 1 };
+                const medal = new Medal(process.env.KEY);
 
-    // Send out the latest tracked clip to the webhook.
-    console.log('Latest clip ID: ', firstClipObj.contentId);
-    discord.send(firstClipObj.directClipUrl);
+                (async () => {
+                    const i = await medal.latest(options);
+                    let firstClipObj = i.data.contentObjects[0];
 
-    setInterval(() => {
-        medal.latest(options).then((r) => {
-            let clipNew = r.data.contentObjects[0];
+                    // Send out the latest tracked clip to the webhook.
+                    console.log('Latest clip ID: ', firstClipObj.contentId);
+                    discord.send(firstClipObj.directClipUrl);
 
-            if (clipNew.contentId !== firstClipObj.contentId) {
-                firstClipObj = clipNew;
-                console.log('New clip detected: ', clipNew.directClipUrl);
-                discord.send(clipNew.directClipUrl);
+                    setInterval(() => {
+                        medal.latest(options).then((r) => {
+                            let clipNew = r.data.contentObjects[0];
+
+                            if (clipNew.contentId !== firstClipObj.contentId) {
+                                firstClipObj = clipNew;
+                                console.log('New clip detected: ', clipNew.directClipUrl);
+                                discord.send(clipNew.directClipUrl);
+                            }
+                        });
+                    }, Number(process.env.INTERVAL) || 30 * 1000);
+                })();
+            } catch (error) {
+                console.log(error)
             }
+
         });
-    }, Number(process.env.INTERVAL) || 30 * 1000);
-})();
+    });
+});
+
